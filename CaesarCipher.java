3bc927598c1f17e904db48eb74fac260ee81fbd4
Roweks <*sam.rowekamp@gmail.com*>
2019-01-31 17:49:37 -0600

    Fix VigenereCipher and add comments

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,61 +1,65 @@
 package Assignment1;
 
 import java.io.PrintWriter;
+
 public class CaesarCipher {
-    public static void main(String[] args) throws Exception {
-        encode("Helloworld");
-        decode("dahhksknhz");
-    }
-    public static void encode(String str) throws Exception {
-        PrintWriter pen = new PrintWriter(System.out, true);
-        for (int i = 0; i < 26;i++) {
-            String newStr = new String("");
-            for (int j = 0; j < (str.length()); j++) {
-                int num = (int)str.charAt(j);
-                if (num >= 97) {
-                    num -= 97;
-                    num = (num + i)%26;
-                    num = num +97;
-                }
-                else if (num >= 65) {
-                    num -= 65;
-                    num = (num + i)%26;
-                    num = num + 65;
-                }
-                char ch = (char)num;
-                newStr += ch;
-            }
-            pen.println("n = " + i + ": " + newStr);
+  public static void main(String[] args) throws Exception {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    // Check inputs for correctness and to run methods
+    if (args.length != 2)
+      pen.println("Incorrect number of parameters");
+    else {
+      String command = args[0];
+      if (command.equals("encode"))
+        encode(args[1]);
+      else if (command.equals("decode"))
+        decode(args[1]);
+      else
+        pen.println("Valid options are \"encode\" or \"decode\"");
+    } // else
+  } // main
+
+  // Method to encrypt an input
+  public static void encode(String str) throws Exception {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    // Encrypt for each letter
+    for (int i = 0; i < 26; i++) {
+      String newStr = new String("");
+      // Encrypt each letter of the string
+      for (int j = 0; j < (str.length()); j++) {
+        int num = (int) str.charAt(j);
+        num -= 97;
+        num = (num + i) % 26;
+        num = num + 97;
+        char ch = (char) num;
+        newStr += ch;
+      } // inner for
+      pen.println("n = " + i + ": " + newStr);
+    } // outer for
+    pen.flush();
+  } // method encode
+
+  // Method to decrypt string
+  public static void decode(String str) throws Exception {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    // Decrypt for every letter
+    for (int i = 0; i < 26; i++) {
+      String newStr = new String("");
+      // Decrypt each character
+      for (int j = 0; j < (str.length()); j++) {
+        int num = (int) str.charAt(j);
+        num -= 97 + i;
+        if (num > 0) {
+          num = num % 26;
+        } else if (num < 0) {
+          num = 26 + num;
         }
-        pen.flush();
-    }
-    public static void decode(String str) throws Exception {
-        PrintWriter pen = new PrintWriter(System.out, true);
-        for (int i = 0; i < 26;i ++) {
-             String newStr = new String("");
-            for (int j = 0; j < (str.length()); j++) {
-                int num = (int)str.charAt(j);
-                if (num >= 97) {
-                    num -= 97 + i;
-                    if (num > 0) {
-                        num = num%26;}
-                    else if (num < 0) {
-                        num = 26 + num;}
-                    num = num + 97;
-                }
-                else if (num >= 65) {
-                    num -= 65 + i;
-                    if (num > 0) {
-                        num = num%26;}
-                    else if (num < 0) {
-                        num = 26 + num;}
-                    num = num + 65;
-                }
-                char ch = (char)num;
-                newStr += ch;
-            }
-            pen.println("n = " + i + ": " + newStr);
-        }
-        pen.flush();
-    }           
+        num = num + 97;
+        char ch = (char) num;
+        newStr += ch;
+      } // inner for
+      pen.println("n = " + i + ": " + newStr);
+    } // outer for
+    pen.flush();
+  } // decode
 }
--- VigenereCipher.java
+++ VigenereCipher.java
@@ -4,56 +4,54 @@ import java.io.PrintWriter;
 
 public class VigenereCipher {
   public static void main(String[] args) {
-    encode("helloworld", "cap");
-    decode("jeanolqraf", "cap");
-  }
+    PrintWriter pen = new PrintWriter(System.out, true);
+    // Check inputs for correctness and method choice
+    if (args.length != 3)
+      pen.println("Incorrect number of parameters");
+    else {
+      String command = args[0];
+      if (command.equals("encode"))
+        encode(args[1], args[2]);
+      else if (command.equals("decode"))
+        decode(args[1], args[2]);
+      else
+        pen.println("Valid options are \"encode\" or \"decode\"");
+    } // else
+  } // main
 
+  // Method to encrypt string with given key
   public static void encode(String str, String key) {
     PrintWriter pen = new PrintWriter(System.out, true);
     String newStr = new String("");
+    // Iterate through the string
     for (int i = 0; i < str.length(); i++) {
       int num = (int) str.charAt(i);
       int numKey = (int) key.charAt(i % key.length());
-      if (num >= 97) {
-        num -= 97;
-        numKey -= 97;
-        num = (num + numKey) % 26;
-        num = num + 97;
-      } else if (num >= 65) {
-        num -= 65;
-        numKey -= 65;
-        num = (num + numKey) % 26;
-        num = num + 65;
-      }
+      num -= 97;
+      numKey -= 97;
+      num = (num + numKey) % 26;
+      num = num + 97;
       char ch = (char) num;
       newStr += ch;
     }
     pen.println(newStr);
-  }
+  } // method encode
 
+  // Method to decrypt a string with a given key
   public static void decode(String str, String key) {
     PrintWriter pen = new PrintWriter(System.out, true);
     String newStr = new String("");
+    // Iterate through the string
     for (int i = 0; i < str.length(); i++) {
       int num = (int) str.charAt(i);
       int numKey = (int) key.charAt(i % key.length());
-      if (num >= 97) {
-        num -= numKey;
-        if (num > 0) {
-          num = num % 26;
-        } else if (num < 0) {
-          num = 26 + num;
-        }
-        num = num + 97;
-      } else if (num >= 65) {
-        num -= numKey;
-        if (num > 0) {
-          num = num % 26;
-        } else if (num < 0) {
-          num = 26 + num;
-        }
-        num = num + 65;
+      num -= numKey;
+      if (num > 0) {
+        num = num % 26;
+      } else if (num < 0) {
+        num = 26 + num;
       }
+      num = num + 97;
       char ch = (char) num;
       newStr += ch;
     }
